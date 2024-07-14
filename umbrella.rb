d4ce258bc38088a0f944cf6c7f3d530f8211d0f6
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-07-14 19:45:00 +0000

    Added temp and precip

--- umbrella.rb
+++ umbrella.rb
@@ -1,22 +1,24 @@
-pp "howdy"
+require "http"
+require "json"
 
-pp "Where are you located?"
+line_width = 35
+
+puts "-" * line_width
+puts "Will you need an umbrella today?"
+puts "-" * line_width
+puts "Where are you located?"
 
 user_location = gets.chomp.gsub(" ", "%20")
-
 # user_location = "Chicago"
 
-pp user_location
+puts "Checking the weather in #{user_location}..."
+
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
-
-require "http"
 
 resp = HTTP.get(maps_url)
 
 raw_response = resp.to_s
-
-require "json"
 
 parsed_response = JSON.parse(raw_response) 
 
@@ -28,28 +30,60 @@ geo = first_result.fetch("geometry")
 
 loc = geo.fetch("location")
 
-pp latitude = loc.fetch("lat")
-pp longitude = loc.fetch("lng")
+latitude = loc.fetch("lat")
+longitude = loc.fetch("lng")
+
+puts "Your coordinates are #{latitude}, #{longitude}."
 
 # I've already created a string variable above: pirate_weather_api_key
 # It contains sensitive credentials that hackers would love to steal so it is hidden for security reasons.
 
-require "http"
-
 pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
 # Assemble the full URL string by adding the first part, the API token, and the last part together
-pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
+pirate_weather_url = "https://api.pirateweather.net/forecast/#{pirate_weather_api_key}/#{latitude},#{longitude}"
 
 # Place a GET request to the URL
 raw_response = HTTP.get(pirate_weather_url)
-
-require "json"
 
 parsed_response = JSON.parse(raw_response)
 
 currently_hash = parsed_response.fetch("currently")
 
 current_temp = currently_hash.fetch("temperature")
+minutely_hash = parsed_response.fetch("minutely", false)
 
-#puts "The current temperature is " + current_temp.to_s + "."
+puts "The current temperature is " + current_temp.to_s + "°F."
+
+if minutely_hash
+  next_hour_summary = minutely_hash.fetch("summary")
+  puts "Next hour: #{next_hour_summary}"
+end
+
+hourly_hash = parsed_response.fetch("hourly")
+hourly_data_array = hourly_hash.fetch("data")
+next_twelve_hours = hourly_data_array[1..12]
+precip_prob_threshold = 0.10
+any_precipitation = false
+
+
+next_twelve_hours.each do |hour_hash|
+  precip_prob = hour_hash.fetch("precipProbability") 
+
+  if precip_prob > precip_prob_threshold
+    any_precipitation = true
+
+    precip_time = Time.at(hour_hash.fetch("time"))
+
+    seconds_from_now = precip_time - Time.now
+
+    hours_from_now = seconds_from_now / 60 / 60
+
+    puts "In #{hours_from_now.round} hours, there is a #{(precip_prob * 100).round}% chance of rain."
+  end
+end
+  if any_precipitation == true
+    puts "You might wanna bring an umbrella."
+  else
+    puts "You don't need an umbrella today."
+  end
